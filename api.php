97a38aa28e995a827b2fbd42a8f082e67c45a9d2
lucas1111 <1848912913@qq.com>
2016-05-14 23:14:38 +0800

    no message

--- api.php
+++ api.php
@@ -2,7 +2,10 @@
   //require_once "app/QueryPhone.php";
 require_once "autoload.php";
 
-	app\TelQuery::query('111');
-	app\TelQuery::query('18206033456');
+use app\TelQuery;
+//use fun\HttpCheckRequset;
+	//app\TelQuery::query('111');
+	TelQuery::query('18206033456');
+	//HttpCheckRequset::testRequset();
 	//echo "\nnext\n";
     //app\TelQuery::test();
--- app/TelQuery.php
+++ app/TelQuery.php
@@ -1,13 +1,23 @@
-<?php
-namespace app;
+<?php namespace app;
+
+use fun\HttpCheckRequest;
 
 //类名与文件名一致
 class TelQuery{
+	const PHONE_API = 'https://tcc.taobao.com/cc/json/mobile_tel_segment.htm';
+
 	public static function query($phoneNum){
 		//var_dump(preg_match('/^0?1[3|4|5|7|8][0-9]\d{8}$/', $phoneNum));
-		var_dump(self::checkTel($phoneNum));
+		//var_dump(self::checkTel($phoneNum));
+		if(self::checkTel($phoneNum)){
+			//var_dump('TelQuery_query');
+			//HttpCheckRequest::testRequest();
+			//var_dump(HttpCheckRequest::request(self::PHONE_API,['tel'=>$phoneNum]));
+			HttpCheckRequest::request(self::PHONE_API,['tel'=>$phoneNum]);
+		}
 	}
 
+/**校验手机号码**/
 	public static function checkTel($phoneNum){
 		if(preg_match('/^0?1[3|4|5|7|8][0-9]\d{8}$/', $phoneNum)){
 			return true;
@@ -32,23 +42,23 @@ class TelQuery{
 非贪婪匹配
 
 贪婪匹配
-在满足匹配时，匹配尽可能长的字符串，默认情况下，采用贪婪匹配
-string pattern1 = @"a.*c";  // greedy match 
-Regex regex = new Regex(pattern1);
-regex.Match("abcabc"); // return "abcabc"
+	在满足匹配时，匹配尽可能长的字符串，默认情况下，采用贪婪匹配
+	string pattern1 = @"a.*c";  // greedy match 
+	Regex regex = new Regex(pattern1);
+	regex.Match("abcabc"); // return "abcabc"
 
 非贪婪匹配
-在满足匹配时，匹配尽可能短的字符串，使用?来表示非贪婪匹配
-string pattern1 = @"a.*?c";  // non-greedy match 
-Regex regex = new Regex(pattern1);
-regex.Match("abcabc"); // return "abc"
+	在满足匹配时，匹配尽可能短的字符串，使用?来表示非贪婪匹配
+	string pattern1 = @"a.*?c";  // non-greedy match 
+	Regex regex = new Regex(pattern1);
+	regex.Match("abcabc"); // return "abc"
 
 几个常用的非贪婪匹配Pattern
-*? 重复任意次，但尽可能少重复
-+? 重复1次或更多次，但尽可能少重复
-?? 重复0次或1次，但尽可能少重复
-{n,m}? 重复n到m次，但尽可能少重复
-{n,}? 重复n次以上，但尽可能少重复
+	*? 重复任意次，但尽可能少重复
+	+? 重复1次或更多次，但尽可能少重复
+	?? 重复0次或1次，但尽可能少重复
+	{n,m}? 重复n到m次，但尽可能少重复
+	{n,}? 重复n次以上，但尽可能少重复
 
 不捕捉模式
 	如何关闭圆括号的捕获能力？而只是用它来做分组，方法是在左括号的后边加上:?，这里第一个圆括弧只是用来分组，而不会占用捕获变量，所以$1的内容只能是steak或者burger，而永远不可能是bronto。
